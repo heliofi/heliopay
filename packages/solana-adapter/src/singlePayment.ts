30585f0d65957273e83f56e8b6c5b2d9f0f9b6d4
Ivan Britvic <ivbritvic@gmail.com>
2022-09-09 12:22:37 +0200

    refactor: remove direct single payments

--- packages/solana-adapter/src/singlePayment.ts
+++ packages/solana-adapter/src/singlePayment.ts
@@ -1,25 +1,54 @@
-import {
-  Transaction,
-  sendAndConfirmRawTransaction,
-  Connection,
-} from '@solana/web3.js';
+import { PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY } from '@solana/web3.js';
 import {
   ASSOCIATED_TOKEN_PROGRAM_ID,
   Token,
   TOKEN_PROGRAM_ID,
 } from '@solana/spl-token';
-import { Wallet } from '@project-serum/anchor/src/provider';
-import { SinglePaymentRequest } from './types';
+import { BN, Program } from '@project-serum/anchor';
+import { HelioIdl } from './program';
+import { Account, SinglePaymentRequest } from './types';
 import './config';
 
-export const singlePayment = async (
-  connection: Connection,
-  wallet: Wallet,
-  req: SinglePaymentRequest
-): Promise<any> => {
-  console.error('Use payment over SC, this is to slow!');
+const prepareSplitPaymentsValues = (
+  amounts: Array<number> = [],
+  accounts: Array<PublicKey> = []
+): { remainingAmounts: Array<BN>; remainingAccounts: Array<Account> } => {
+  if (!amounts.length) {
+    if (accounts.length > 0) {
+      throw new Error('Remaining accounts when no amounts!');
+    }
+  } else if (2 * amounts.length !== accounts.length) {
+    throw new Error('Remaining accounts not matching amounts!');
+  }
+
+  const remainingAmounts = [];
+  const remainingAccounts = [];
+
+  for (let i = 0; i < amounts.length; i++) {
+    remainingAmounts.push(new BN(amounts[i]));
+    remainingAccounts.push({
+      pubkey: accounts[2 * i],
+      isWritable: false,
+      isSigner: false,
+    });
+    remainingAccounts.push({
+      pubkey: accounts[2 * i + 1],
+      isWritable: true,
+      isSigner: false,
+    });
+  }
+
+  return { remainingAmounts, remainingAccounts };
+};
+
+export const singlePaymentSC = async (
+  program: Program<HelioIdl>,
+  req: SinglePaymentRequest,
+  amounts: Array<number> = [],
+  accounts: Array<PublicKey> = []
+): Promise<string> => {
   const mint = req.mintAddress;
-  // Associated token accounts
+
   const senderAssociatedTokenAddress = await Token.getAssociatedTokenAddress(
     ASSOCIATED_TOKEN_PROGRAM_ID,
     TOKEN_PROGRAM_ID,
@@ -34,24 +63,23 @@ export const singlePayment = async (
     req.recipient
   );
 
-  const transaction = new Transaction().add(
-    Token.createTransferInstruction(
-      TOKEN_PROGRAM_ID,
-      senderAssociatedTokenAddress,
-      recipientAssociatedTokenAddress,
-      req.sender,
-      [],
-      req.amount
-    )
+  const { remainingAmounts, remainingAccounts } = prepareSplitPaymentsValues(
+    amounts,
+    accounts
   );
 
-  // Find latest block and add to transaction
-  const blockHash = await connection.getLatestBlockhash();
-  transaction.feePayer = wallet.publicKey;
-  transaction.recentBlockhash = await blockHash.blockhash;
-
-  // Sign and prepare
-  const signed = await wallet.signTransaction(transaction);
-  const tx = await sendAndConfirmRawTransaction(connection, signed.serialize());
-  return { transaction: tx };
+  return program.rpc.singlePayment(new BN(req.amount), remainingAmounts, {
+    accounts: {
+      sender: req.sender,
+      senderTokenAccount: senderAssociatedTokenAddress,
+      recipient: req.recipient,
+      recipientTokenAccount: recipientAssociatedTokenAddress,
+      mint,
+      rent: SYSVAR_RENT_PUBKEY,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      systemProgram: SystemProgram.programId,
+    },
+    remainingAccounts,
+  });
 };
