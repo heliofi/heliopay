e64d6f3c6565dece8fd86041dd772aa228b02409
Muhammed Erdem <muhammed@erdem.dev>
2022-10-17 18:14:51 +0300

    fix: phone number css import removed

--- packages/react/src/components/phoneNumberInput/index.tsx
+++ packages/react/src/components/phoneNumberInput/index.tsx
@@ -1,6 +1,5 @@
 import PhoneInput from 'react-phone-number-input';
 
-import 'react-phone-number-input/style.css';
 import InputContainer from '../input-container';
 import { StyledErrorMessage, StyledLabel, StyledWrapper } from './styles';
 
