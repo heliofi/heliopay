dc15053fb47062752951a442a994384ebef85fbf
Ivan Britvic <ivbritvic@gmail.com>
2023-01-23 11:26:20 +0100

    refactor: change abi location

--- packages/evm-adapter/src/index.ts
+++ packages/evm-adapter/src/index.ts
@@ -4,4 +4,4 @@ export * from './getEthPaymentTx';
 export * from './getSplitEthPaymentTx';
 export * from './types';
 export * from './constants';
-export * from './abi';
+export * from './abi/helio';
