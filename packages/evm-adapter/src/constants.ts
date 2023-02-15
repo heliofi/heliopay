0cdbff7f8cd1a4ff4c4087f2d1d5d6ad850e7697
ibrica <ivbritvic@gmail.com>
2023-02-15 16:22:19 +0100

    feat: mainnet address

--- packages/evm-adapter/src/constants.ts
+++ packages/evm-adapter/src/constants.ts
@@ -2,7 +2,7 @@ import { BigNumber, utils } from 'ethers';
 
 export enum contractAddresses {
   POLYGON_MUMBAI = '0xa35e123aA4AFeD2E4957897232F2ABf73f34cfB0',
-  POLYGON_MAINNET = '0xa35e123aA4AFeD2E4957897232F2ABf73f34cfB0',
+  POLYGON_MAINNET = '0x5a77CE11bCF74E07F2760d610Ba0Aa1DF6887a48',
 }
 
 export const gasLimit = BigNumber.from(utils.hexlify('0x100000'));
