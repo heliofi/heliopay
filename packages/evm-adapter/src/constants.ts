5ab6cd2fc43a571dd41b19b4d51d2ff45457e02f
ibrica <ivbritvic@gmail.com>
2023-02-15 18:04:34 +0100

    fix: contract addresses

--- packages/evm-adapter/src/constants.ts
+++ packages/evm-adapter/src/constants.ts
@@ -1,12 +1,8 @@
 import { BigNumber, utils } from 'ethers';
 
-// For compability with BE and FE, remove in future
-export const contractAddress = '0xa35e123aA4AFeD2E4957897232F2ABf73f34cfB0';
-
 export enum ContractAddresses {
-  POLYGON_MUMBAI = '0xa35e123aA4AFeD2E4957897232F2ABf73f34cfB0',
-  POLYGON_MAINNET = '0xa35e123aA4AFeD2E4957897232F2ABf73f34cfB0',
-  // POLYGON_MAINNET = '0x5a77CE11bCF74E07F2760d610Ba0Aa1DF6887a48',
+  POLYGON_MUMBAI = '0x1B43FB8a3c4CC150768C72656C352bdf35Dd1209',
+  POLYGON_MAINNET = '0x644cF27D0CB17cf252BCfb96e2821fc17109761f',
 }
 
 export const gasLimit = BigNumber.from(utils.hexlify('0x100000'));
