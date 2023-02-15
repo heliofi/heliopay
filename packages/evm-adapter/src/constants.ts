8aae408cff7ddd8f95084f5b3691ef1c1ba32dda
ibrica <ivbritvic@gmail.com>
2023-02-15 13:53:41 +0100

    fix: token approval

--- packages/evm-adapter/src/constants.ts
+++ packages/evm-adapter/src/constants.ts
@@ -1,8 +1,8 @@
 import { BigNumber, utils } from 'ethers';
 
 export enum contractAddresses {
-  POLYGON_MUMBAI = '0x707Cad6EDB79eE9F64818B09F14A7Aa7f4910e93',
-  POLYGON_MAINNET = '0x707Cad6EDB79eE9F64818B09F14A7Aa7f4910e93',
+  POLYGON_MUMBAI = '0xa35e123aA4AFeD2E4957897232F2ABf73f34cfB0',
+  POLYGON_MAINNET = '0xa35e123aA4AFeD2E4957897232F2ABf73f34cfB0',
 }
 
 export const gasLimit = BigNumber.from(utils.hexlify('0x100000'));
