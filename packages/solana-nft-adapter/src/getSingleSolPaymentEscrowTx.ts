8f7b7f66101537059a5bb78e87c72fd0ae64505d
ibrica <ivbritvic@gmail.com>
2023-03-30 15:23:01 +0200

    feat: single sol payment escrow

--- packages/solana-nft-adapter/src/getSingleSolPaymentEscrowTx.ts
+++ packages/solana-nft-adapter/src/getSingleSolPaymentEscrowTx.ts
@@ -14,36 +14,16 @@ import { HelioNftIdl } from './program';
 import { SinglePaymentRequest } from './types';
 import { helioFeeWalletKey, daoFeeWalletKey } from './config';
 
-export const getSinglePaymentTx = async (
+export const getSingleSolPaymentEscrowTx = async (
   program: Program<HelioNftIdl>,
   req: SinglePaymentRequest,
   fee: number = 0
 ): Promise<Transaction> => {
-  const currency = req.currency;
   const escrowAccount = req.escrowAccount;
-
-  const senderTokenAccount = await getAssociatedTokenAddress(
-    currency,
-    req.sender
-  );
 
   const senderNftAccount = await getAssociatedTokenAddress(
     req.nftMint,
     req.sender
-  );
-  const recipientTokenAccount = await getAssociatedTokenAddress(
-    currency,
-    req.recipient
-  );
-
-  const helioFeeTokenAccount = await getAssociatedTokenAddress(
-    currency,
-    helioFeeWalletKey
-  );
-
-  const daoFeeTokenAccount = await getAssociatedTokenAddress(
-    currency,
-    daoFeeWalletKey
   );
 
   const escrowNftAccount = await getAssociatedTokenAddress(
@@ -57,22 +37,17 @@ export const getSinglePaymentTx = async (
   );
 
   return await program.methods
-    .singlePayment(new BN(req.amount), new BN(fee))
+    .singleSolPaymentEscrow(new BN(req.amount), new BN(fee))
     .accounts({
       sender: req.sender,
-      senderTokenAccount,
       senderNftAccount,
       recipient: req.recipient,
-      recipientTokenAccount,
       escrowAccount,
       escrowNftAccount,
       escrowPda,
       helioFeeAccount: helioFeeWalletKey,
       daoFeeAccount: daoFeeWalletKey,
-      helioFeeTokenAccount,
-      daoFeeTokenAccount,
       mint: req.nftMint,
-      currency,
       rent: SYSVAR_RENT_PUBKEY,
       tokenProgram: TOKEN_PROGRAM_ID,
       associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
