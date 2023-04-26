aa3c9e85ac5a6fc7b91677af090d8023f4654116
suren <suren@fifth-llc.com>
2023-04-26 20:13:17 +0400

    fix: import image from aws s3

--- packages/react/src/ui-kits/phantomCompatible/index.tsx
+++ packages/react/src/ui-kits/phantomCompatible/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import { StyledPhantomLogo, StyledPhantomRow } from './styles';
 
-// @ts-ignore @todo-v configure files paths
-import PhantomPurple from '../../../public/PhantomPurple.png';
 
 const PhantomCompatibleCard = () => (
   <StyledPhantomRow>
     <StyledPhantomLogo>
-      <img src={PhantomPurple} alt="Phantom purple" />
+      <img src='https://helio-assets.s3.eu-west-1.amazonaws.com/PhantomPurple.png' alt="Phantom purple" />
     </StyledPhantomLogo>
     <p>Compatible with Phantom wallet</p>
   </StyledPhantomRow>
