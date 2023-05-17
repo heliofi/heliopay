b16a16774019bbcf7d66c70eb5c7689c62fe8c7a
SurenAvagyanG <suren@fifth-llc.com>
2023-05-17 15:06:58 +0400

    feat: merge main into current branch

--- packages/sdk/index.ts
+++ packages/sdk/index.ts
@@ -1,7 +1,3 @@
 import 'reflect-metadata';
 
-export * from './public-api';
-export * from './src/infrastructure';
-export * from './src/app';
-export * from './src/domain';
-export * from './src/utils';
+export * from './src';
