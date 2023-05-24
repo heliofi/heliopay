0b13cd64f1244ceabe62bd825e80a705fade5124
00MB <michaelwbeer@gmail.com>
2023-05-24 09:57:02 +0100

    fix: added linear issue

--- packages/react/src/ui-kits/phantomCompatible/index.tsx
+++ packages/react/src/ui-kits/phantomCompatible/index.tsx
@@ -4,6 +4,7 @@ import { StyledPhantomLogo, StyledPhantomRow } from './styles';
 
 // @ts-ignore @todo-v configure files paths
 // import PhantomPurple from '../../../public/PhantomPurple.png';
+// TODO: Investigate this issue, causing build failure: https://linear.app/heliofi/issue/FEE-1125/react-button-phantom-logo-breaking-build
 
 const PhantomCompatibleCard = () => (
   <StyledPhantomRow>
